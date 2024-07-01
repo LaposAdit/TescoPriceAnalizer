3407eac128cec76e92c7962ca7437fe4217f29d6
Tibor Lapos <lapos@aconsulting.sk>
2024-07-01 11:01:31 +0200

    website

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "flowbite-react";
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
